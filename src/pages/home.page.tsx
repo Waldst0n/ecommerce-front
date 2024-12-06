b2d0f7017a1676aa5c68f5ca1d98091ee309b00d
Waldston <waldston.ws@gmail.com>
2024-12-06 20:33:51 -0300

    style: change Home Page div to fragment

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -3,10 +3,10 @@ import Header from '../components/header/header.component';
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Header />
       <Categories />
-    </div>
+    </>
   );
 };
 
