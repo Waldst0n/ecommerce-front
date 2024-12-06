d0f4e3acfb64852b43019ba5848e83e596198bcd
Waldston <waldston.ws@gmail.com>
2024-12-06 20:36:18 -0300

    feat: add mock with categories and  styles on css

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -2,19 +2,43 @@ import { useEffect, useState } from 'react';
 import './categories.styles.css';
 import '../../types/category.types';
 import Category from '../../types/category.types';
+import CategoryItem from '../category-item/categoryItem.component';
 
 let mockCategory: Category[] = [
   {
     id: '1',
-    name: 'sneakers',
-    displayName: 'Tênis',
-    imageUrl: 'https://imgcentauro-a.akamaihd.net/1366x1366/98140351.jpg',
+    name: 'cap',
+    displayName: 'Bonés',
+    imageUrl:
+      'https://img.freepik.com/free-photo/view-composition-with-neatly-arranged-organized-sport-items_23-2150275227.jpg?semt=ais_hybrid',
   },
   {
     id: '2',
     name: 'jackets',
+    displayName: 'Tênis',
+    imageUrl:
+      'https://wallpapers-clan.com/wp-content/uploads/2024/09/nike-sneakers-pattern-desktop-wallpaper-cover.jpg',
+  },
+  {
+    id: '3',
+    name: 'sneakers',
     displayName: 'Jaquetas',
-    imageUrl: 'https://imgcentauro-a.akamaihd.net/1366x1366/98140351.jpg',
+    imageUrl:
+      'https://images.unsplash.com/photo-1604644401890-0bd678c83788?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8amFja2V0c3xlbnwwfHwwfHx8MA%3D%3D',
+  },
+  {
+    id: '4',
+    name: 'jackets',
+    displayName: 'Feminino',
+    imageUrl:
+      'https://images.unsplash.com/photo-1603023436454-c91a5d12b580?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmV0cmF0byUyMGRhJTIwbXVsaGVyfGVufDB8fDB8fHww',
+  },
+  {
+    id: '5',
+    name: 'jackets',
+    displayName: 'Masculino',
+    imageUrl:
+      'https://media.istockphoto.com/id/1300966679/pt/foto/young-handsome-man-in-classic-suit-over-the-lake-background.jpg?s=612x612&w=0&k=20&c=5Vzcp9ne1hjadJ-2RKIhjqqUfrLHZVJoaQ9OCL7eGY0=',
   },
 ];
 
@@ -30,8 +54,7 @@ const Categories = () => {
       <div className="categories-content">
         {categories.map((category) => (
           <div key={category.id}>
-            <img src={category.imageUrl} alt="" />
-            <h1>{category.name}</h1>
+            <CategoryItem category={category} />
           </div>
         ))}
       </div>
