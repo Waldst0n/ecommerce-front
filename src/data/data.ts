fad0bcbbb87ab50c92d9aa74c3956ad514bfa1b6
Waldston <waldston.ws@gmail.com>
2024-12-07 08:52:40 -0300

    feat: Add data mock from a separated file

---
+++ src/data/data.ts
@@ -0,0 +1,41 @@
+import Category from '../types/category.types';
+
+let mockCategory: Category[] = [
+  {
+    id: '1',
+    name: 'cap',
+    displayName: 'Bonés',
+    imageUrl:
+      'https://img.freepik.com/free-photo/view-composition-with-neatly-arranged-organized-sport-items_23-2150275227.jpg?semt=ais_hybrid',
+  },
+  {
+    id: '2',
+    name: 'jackets',
+    displayName: 'Tênis',
+    imageUrl:
+      'https://wallpapers-clan.com/wp-content/uploads/2024/09/nike-sneakers-pattern-desktop-wallpaper-cover.jpg',
+  },
+  {
+    id: '3',
+    name: 'sneakers',
+    displayName: 'Jaquetas',
+    imageUrl:
+      'https://images.unsplash.com/photo-1604644401890-0bd678c83788?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8amFja2V0c3xlbnwwfHwwfHx8MA%3D%3D',
+  },
+  {
+    id: '4',
+    name: 'jackets',
+    displayName: 'Feminino',
+    imageUrl:
+      'https://images.unsplash.com/photo-1603023436454-c91a5d12b580?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmV0cmF0byUyMGRhJTIwbXVsaGVyfGVufDB8fDB8fHww',
+  },
+  {
+    id: '5',
+    name: 'jackets',
+    displayName: 'Masculino',
+    imageUrl:
+      'https://media.istockphoto.com/id/1300966679/pt/foto/young-handsome-man-in-classic-suit-over-the-lake-background.jpg?s=612x612&w=0&k=20&c=5Vzcp9ne1hjadJ-2RKIhjqqUfrLHZVJoaQ9OCL7eGY0=',
+  },
+];
+
+export default mockCategory;
